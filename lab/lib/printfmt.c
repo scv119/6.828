328d10e6e1f1d5a29629a3168db5fa6bb2ef30e7
scv119 <scv119@gmail.com>
2016-01-05 09:06:50 -0800

    unsigned

--- lab/lib/printfmt.c
+++ lab/lib/printfmt.c
@@ -205,11 +205,7 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 
 		// (unsigned) octal
 		case 'o':
-      num = getint(&ap, lflag);
-      if ((long long) num < 0) {
-        putch('-', putdat);
-        num = -(long long) num;
-      }
+      num = getuint(&ap, lflag);
       putch('0', putdat);
       base = 8;
       goto number;
